1965c21df138d4fdea88da94c30b0a2b714fe234
Rafael Cancian <cancian.rafa@gmail.com>
2021-01-23 20:42:41 +0000

    refactor consumer fraud detector

--- src/main/java/com/ecommerce/kafka/consumers/fraud/ConsumerFraudDetector.java
+++ src/main/java/com/ecommerce/kafka/consumers/fraud/ConsumerFraudDetector.java
@@ -1,42 +1,20 @@
 package com.ecommerce.kafka.consumers.fraud;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
-
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
+import com.ecommerce.kafka.consumers.ConsumerService;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public class ConsumerFraudDetector {
 
-    public static void execute() {
-
-        var kafkaConsumer = new KafkaConsumer<String, String>(getProperties());
-        kafkaConsumer.subscribe(Collections.singletonList("ECOMMERCE_FRAUD_DETECTOR"));
-
-        Thread thread = new Thread(() -> {
-            System.out.println("Thread of ConsumerFraudDetector");
-            while (true) {
-                var records = kafkaConsumer.poll(Duration.ofSeconds(1));
-                if (!records.isEmpty()) {
-                    records.forEach(record -> {
-                        System.out.println(">> Fraud detector analyzed with success");
-                        System.out.println("Key: " + record.key() + "| Value:" + record.value());
-                    });
-                }
-            }
-        });
-        thread.start();
+    private ConsumerFraudDetector() {
     }
 
-    private static Properties getProperties() {
-        Properties properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9091");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, ConsumerFraudDetector.class.getSimpleName());
-        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
-        return properties;
+    public static void execute() {
+        ConsumerService consumerService = new ConsumerService("ECOMMERCE_FRAUD_DETECTOR", ConsumerFraudDetector::parse);
+        consumerService.execute();
+    }
+
+    public static void parse(ConsumerRecord<String, String> record) {
+        System.out.println(">> Fraud detector analyzed with success");
+        System.out.println("Key: " + record.key() + "| Value:" + record.value());
     }
 }
