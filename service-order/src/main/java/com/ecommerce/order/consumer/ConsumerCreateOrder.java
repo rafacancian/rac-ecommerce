207a08956ee76e42dd4eecd8766d8b65904ccd08
Rafael Cancian <cancian.rafa@gmail.com>
2021-01-25 11:29:18 +0000

    refactor modules - move class to service-order

---
+++ service-order/src/main/java/com/ecommerce/order/consumer/ConsumerCreateOrder.java
@@ -0,0 +1,25 @@
+package com.ecommerce.order.consumer;
+
+import com.ecommerce.kafka.consumers.ConsumerService;
+import com.ecommerce.kafka.models.Order;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.util.Map;
+
+public class ConsumerCreateOrder {
+
+    private ConsumerCreateOrder() {
+    }
+
+    public static void execute() {
+        ConsumerService consumerService = new ConsumerService<Order>("ECOMMERCE_NEW_ORDER",
+                ConsumerCreateOrder.class.getSimpleName(), ConsumerCreateOrder::parse, Order.class, Map.of());
+        consumerService.execute();
+    }
+
+    public static void parse(ConsumerRecord<String, Order> record) {
+        System.out.println(">> Order created with success");
+        System.out.println("Key: " + record.key() + "| Value:" + record.value());
+    }
+
+}
