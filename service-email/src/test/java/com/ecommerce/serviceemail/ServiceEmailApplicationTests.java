48875c947631a25b02d0ae122da63242b8aad061
Rafael Cancian <cancian.rafa@gmail.com>
2021-01-25 10:01:52 +0000

    refactor modules - create service-email

---
+++ service-email/src/test/java/com/ecommerce/serviceemail/ServiceEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.serviceemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
