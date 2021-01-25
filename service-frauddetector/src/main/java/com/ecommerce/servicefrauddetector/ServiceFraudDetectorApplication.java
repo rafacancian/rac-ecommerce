74f14e962ce9467d326b1133df50b944b1a45caf
Rafael Cancian <cancian.rafa@gmail.com>
2021-01-25 10:06:54 +0000

    refactor modules - create service-frauddetector

---
+++ service-frauddetector/src/main/java/com/ecommerce/servicefrauddetector/ServiceFraudDetectorApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.servicefrauddetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceFraudDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceFraudDetectorApplication.class, args);
+	}
+
+}
---
+++ service-frauddetector/src/test/java/com/ecommerce/servicefrauddetector/ServiceFraudDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.servicefrauddetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceFraudDetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
