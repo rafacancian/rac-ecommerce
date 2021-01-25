ecd99d9cda930166a1dfa08003068b1d06452dcb
Rafael Cancian <cancian.rafa@gmail.com>
2021-01-25 10:11:41 +0000

    refactor modules - create service-logger

---
+++ service-logger/src/main/java/com/ecommerce/servicelogger/ServiceLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.servicelogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceLoggerApplication.class, args);
+	}
+
+}
---
+++ service-logger/src/test/java/com/ecommerce/servicelogger/ServiceLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.servicelogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
