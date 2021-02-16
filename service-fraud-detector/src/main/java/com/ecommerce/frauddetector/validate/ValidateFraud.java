8c3c2b87f5520c2a0c091c072e94b125da470cd8
Rafael Cancian <cancian.rafa@gmail.com>
2021-02-16 21:26:12 +0000

    add common kafka module

--- service-fraud-detector/src/main/java/com/ecommerce/frauddetector/validate/ValidateFraud.java
+++ service-fraud-detector/src/main/java/com/ecommerce/frauddetector/validate/ValidateFraud.java
@@ -4,11 +4,15 @@ import com.ecommerce.frauddetector.models.Order;
 
 public class ValidateFraud {
 
+    private ValidateFraud() {
+    }
+
     public static boolean checkIsFraud(Order order) {
         return validatePriceAmountIsFraud(order);
     }
 
     private static boolean validatePriceAmountIsFraud(Order order) {
+
         boolean isFraud = false;
         // simulate a fictitious rule when if price is great than 200, the order is a fraud
         if (order.getPrice() > 200) {
