6b60776e0bb8b499c4ca1ad0f1837c24e6f6594a
Rafael Cancian <cancian.rafa@gmail.com>
2021-02-20 15:25:27 +0000

    update message name to generic ecommerce

--- service-user/src/main/java/com/ecommerce/serviceuser/consumer/ConsumerReport.java
+++ service-user/src/main/java/com/ecommerce/serviceuser/consumer/ConsumerReport.java
@@ -1,8 +1,9 @@
 package com.ecommerce.serviceuser.consumer;
 
 import com.ecommerce.consumer.ConsumerService;
+import com.ecommerce.model.Message;
+import com.ecommerce.model.User;
 import com.ecommerce.producer.ProducerService;
-import com.ecommerce.serviceuser.models.User;
 import com.ecommerce.serviceuser.services.UserService;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.stereotype.Service;
@@ -19,12 +20,12 @@ public class ConsumerReport {
     private ProducerService<User> producerService = new ProducerService<>();
 
     public void execute() {
-        ConsumerService consumerService = new ConsumerService<String>("SEND_MESSAGE_ALL_USERS",
+        ConsumerService consumerService = new ConsumerService<String>("ECOMMERCE_SEND_MESSAGE_ALL_USERS",
                 ConsumerReport.class.getSimpleName(), this::parse, String.class, Map.of());
         consumerService.execute();
     }
 
-    public void parse(ConsumerRecord<String, String> record) {
+    public void parse(ConsumerRecord<String, Message<String>> record) {
         System.out.println(">> Processing batch send message all users");
         System.out.println("Key: " + record.key() + "| Value:" + record.value());
         List<User> users = userService.findAllUsers();
@@ -32,7 +33,7 @@ public class ConsumerReport {
         if (!ObjectUtils.isEmpty(users)) {
             System.out.println("Numbers of user founded: " + users.size());
             for (User user : users) {
-                producerService.send(record.value(), user.getEmail(), user);
+                producerService.send(record.value().getPayload(), user.getEmail(), user);
             }
         }
     }
